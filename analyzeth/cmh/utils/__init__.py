fde3a5acea6be502bfed3c78559901162a4020c0
Cameron Holman cameron.holman@berkeley.edu <cameron.holman@rhino2.rhino.psych.upenn.edu>
2022-04-13 19:03:05 -0400

    cell FR rolling window

---
+++ analyzeth/cmh/utils/__init__.py
@@ -0,0 +1,6 @@
+""" Module - Utility functions for loading NWB, displaying NWB info, subsetting data in epochs of interest """
+
+from .load_nwb import load_nwb
+from .nwb_info import nwb_info
+from .subset_data import subset_period_data, subset_period_event_time_data
+from .cell_firing_rate import cell_firing_rate
---
+++ analyzeth/cmh/utils/cell_firing_rate.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+def cell_firing_rate(
+        spikes,         #ms - may be s now 
+        window = 100,   #ms 
+        step = 10       #ms 
+        ):
+
+    """ Calculate firing rate across epoch 
+    
+    This function will take a 1D spike time array and calculate
+    firing rate at each point in time (governed by step size) 
+    with a rolling window (governed by window size)
+
+    Edges are refelcted based on window size
+
+    Parameters
+    ----------
+    spikes: 1D array
+        time stamps of spikes in ms
+
+    window: int 
+        legth of window over which to calculate FR
+
+    step: int
+        step size for moving window
+
+    Returns
+    -------
+    firing_rates: 1D array
+        firing rates at each point in time
+        length of array depends on step size
+
+    """
+
+    # Normalize spike time to start of spike triain
+    spikes = spikes - spikes[0]
+    
+    # Length of epoch 
+    len_epoch = spikes[-1] 
+
+    # Reflect edges by window size
+    window = int(window) if ((int(window) % 2) == 0) else int(window) + 1     # make window even
+    start_reflection = np.flipud(spikes[spikes < window])
+    end_reflection   = np.flipud(spikes[spikes > len_epoch - window])
+    spikes = np.concatenate([start_reflection, spikes, end_reflection])
+
+    # Iter 
+    num_bins = int(np.ceil(len_epoch/step)) + 1 
+    times = []
+    FRs = []                                       
+    for ix_win in range(num_bins):
+        center = step * ix_win
+        left = center - window/2
+        right = center + window/2
+        spikes_bin = spikes[(left<spikes) & (spikes < right)]
+        fr_bin = len(spikes_bin)/window
+        times.append(center)
+        FRs.append(fr_bin)
+
+    # returning times in epoch time ms
+    return FRs, times
