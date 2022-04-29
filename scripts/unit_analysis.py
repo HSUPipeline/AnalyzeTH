3186b5f8d874eaa2d2e86b9397c7770946056fdc
Tom Donoghue <tdonoghue.research@gmail.com>
2022-04-28 20:00:10 -0400

    code updates unit analysis - use new helpers

--- scripts/unit_analysis.py
+++ scripts/unit_analysis.py
@@ -19,11 +19,14 @@ from spiketools.plts.space import plot_positions, plot_heatmap
 from spiketools.plts.trials import plot_rasters
 from spiketools.plts.data import plot_bar, plot_polar_hist, plot_text
 from spiketools.plts.stats import plot_surrogates
-from spiketools.stats.permutations import zscore_to_surrogates, compute_empirical_pvalue
+from spiketools.plts.annotate import color_pval
+from spiketools.stats.permutations import (compute_surrogate_stats,
+                                           compute_empirical_pvalue, zscore_to_surrogates)
 from spiketools.spatial.occupancy import (compute_occupancy, compute_spatial_bin_edges,
                                           compute_spatial_bin_assignment)
 from spiketools.spatial.information import (compute_spatial_information_2d,
                                             _compute_spatial_information)
+from spiketools.utils.data import get_range
 from spiketools.utils.trials import (epoch_spikes_by_event, epoch_spikes_by_range,
                                      epoch_data_by_range)
 
@@ -35,7 +38,8 @@ import sys
 sys.path.append('../code')
 from utils import select_from_list
 from analysis import calc_trial_frs, get_spike_positions, compute_bin_firing, get_spike_heading
-from target import compute_serial_position_fr, compute_spatial_target_bins
+from target import compute_spatial_target_bins
+from serial import compute_serial_position_fr
 from reports import *
 
 ###################################################################################################
@@ -119,11 +123,6 @@ def main():
         # Get the chest positions
         chest_xs, chest_ys = nwbfile.acquisition['stimuli']['chest_positions'].data[:].T
 
-        # TEMP: Fix for extra chest position
-        if len(chest_xs) > len(chest_trials):
-            chest_xs = chest_xs[:len(chest_trials)]
-            chest_ys = chest_ys[:len(chest_trials)]
-
         ## ANALYZE UNITS
 
         # Get unit information
@@ -195,7 +194,7 @@ def main():
                 # Normalize bin firing by occupancy
                 with warnings.catch_warnings():
                     warnings.simplefilter("ignore")
-                    norm_bin_firing = bin_firing / occ
+                    bin_firing = bin_firing / occ
 
                 # Get head direction for each spike
                 #spike_hds = get_spike_heading(spikes, hd_times, hd_degrees)
@@ -241,7 +240,7 @@ def main():
                 target_info = _compute_spatial_information(target_bins, chest_occupancy)
 
                 # Compute measures for head direction
-                #hd_z_val, hd_p_val = circ_rayleigh(convert_angles(spike_hds))
+                #hd_zstat, hd_pstat = circ_rayleigh(convert_angles(spike_hds))
 
                 ## SURROGATES
 
@@ -251,15 +250,15 @@ def main():
                 sp_surrs = np.zeros(shape=[SURROGATE_SETTINGS['N_SURROGATES'], 4])
                 #hd_surrs = np.zeros(SURROGATE_SETTINGS['N_SURROGATES'])
 
-                for ind, stimes in enumerate(times_shuffle):
+                for ind, shuffle in enumerate(times_shuffle):
 
                     # PLACE
-                    s_spike_xs, s_spike_ys = get_spike_positions(stimes, ptimes, positions)
+                    s_spike_xs, s_spike_ys = get_spike_positions(shuffle, ptimes, positions)
                     place_surrs[ind] = compute_spatial_information_2d(s_spike_xs, s_spike_ys,
                                                                       [x_bin_edges, y_bin_edges], occ)
 
                     # TARGET
-                    s_target_bins = compute_spatial_target_bins(stimes, nav_starts,
+                    s_target_bins = compute_spatial_target_bins(shuffle, nav_starts,
                                                                 chest_openings, chest_trials,
                                                                 ptimes, positions,
                                                                 ANALYSIS_SETTINGS['CHEST_BINS'],
@@ -268,29 +267,26 @@ def main():
 
                     # SERIAL POSITION
                     sp_surrs_frs = compute_serial_position_fr(\
-                        stimes, nav_starts, chest_openings, chest_trials, ptimes, positions)
+                        shuffle, nav_starts, chest_openings, chest_trials, ptimes, positions)
                     sp_surrs[ind] = np.mean(sp_surrs_frs, 0)
 
                     # HEAD DIRECTION
-                    #s_spike_hds = get_spike_heading(stimes, hd_times, hd_degrees)
+                    #s_spike_hds = get_spike_heading(shuffle, hd_times, hd_degrees)
                     #hd_surrs[ind] = circ_rayleigh(convert_angles(s_spike_hds))[0]
 
 
                 # Place surrogate measures
-                place_surr_p_val = compute_empirical_pvalue(place_info, place_surrs)
-                place_z_score = zscore_to_surrogates(place_info, place_surrs)
+                place_p_val, place_z_score = compute_surrogate_stats(place_info, place_surrs, False, False)
 
                 # Target surrogate measures
-                target_surr_p_val = compute_empirical_pvalue(target_info, target_surrs)
-                target_z_score = zscore_to_surrogates(target_info, target_surrs)
+                target_p_val, target_z_score = compute_surrogate_stats(target_info, target_surrs, False, False)
 
                 # Serial position surrogate measures
-                sp_surr_p_vals = [compute_empirical_pvalue(sp_all_frs.mean(0)[ind], sp_surrs[:, ind]) for ind in range(4)]
+                sp_p_vals = [compute_empirical_pvalue(sp_all_frs.mean(0)[ind], sp_surrs[:, ind]) for ind in range(4)]
                 sp_z_scores = [zscore_to_surrogates(sp_all_frs.mean(0)[ind], sp_surrs[:, ind]) for ind in range(4)]
 
                 # Head direction surrogate measures
-                #hd_surr_p_val = compute_empirical_pvalue(hd_z_val, hd_surrs)
-                #hd_z_score = zscore_to_surrogates(hd_z_val, hd_surrs)
+                #hd_p_val, hd_z_score = compute_surrogate_stats(hd_zstat, hd_surrs, False, False)
 
                 # Collect information of interest
                 unit_info = create_unit_info(nwbfile.units[unit_ind])
@@ -320,7 +316,7 @@ def main():
                              vline=0, figsize=(10, 7), ax=ax10)
                 title_str = 'All Trials - Pre: {:1.2f} - Pos: {:1.2f}  (t:{:1.2f}, p:{:1.2f})'
                 ax10.set_title(title_str.format(fr_pre_all, fr_post_all, fr_t_val_all, fr_p_val_all),
-                               color='red' if fr_p_val_all < 0.05 else 'black')
+                               color=color_pval(fr_p_val_all))
 
                 # 12&22: Compare Empty & Full chest trials
                 title_str = '{} Chests - Pre: {:1.2f} - Pos: {:1.2f}'
@@ -328,12 +324,13 @@ def main():
                 ax12 = plt.subplot(grid[1, 2])
                 plot_rasters(empty_trials, xlim=ANALYSIS_SETTINGS['TRIAL_RANGE'], vline=0, ax=ax12)
                 ax12.set_title(title_str.format('Empty', fr_pre_empt, fr_post_empt),
-                               color='red' if fr_p_val_empt < 0.05 else 'black')
+                               color=color_pval(fr_p_val_empt))
+
                 # Full chest trials
                 ax22 = plt.subplot(grid[2, 2])
                 plot_rasters(full_trials, xlim=ANALYSIS_SETTINGS['TRIAL_RANGE'], vline=0, ax=ax22)
                 ax22.set_title(title_str.format('Full', fr_pre_full, fr_post_full),
-                               color='red' if fr_p_val_full < 0.05 else 'black')
+                               color=color_pval(fr_p_val_full))
 
                 # ax30: positional firing
                 ax30 = plt.subplot(grid[3:5, 0])
@@ -343,27 +340,25 @@ def main():
 
                 # ax31: positional heatmap
                 ax31 = plt.subplot(grid[3:5, 1])
-                plot_heatmap(norm_bin_firing, transpose=True, ax=ax31,
-                             title='Range: {:1.2f}-{:1.2f}'.format(np.nanmin(norm_bin_firing), np.nanmax(norm_bin_firing)))
+                plot_heatmap(bin_firing, transpose=True, ax=ax31,
+                             title='Range: {:1.2f}-{:1.2f}'.format(*get_range(bin_firing)))
 
                 # # ax31: head direction of spike firing
                 # ax32 = plt.subplot(grid[3, 2], polar=True)
                 # plot_polar_hist(spike_hds, ax=ax32)
                 # ax32.set(xticklabels=[], yticklabels=[])
-                # tcol32 = 'red' if hd_surr_p_val < 0.05 else 'black'
-                # ax32.set_title('Head Direction', color=tcol32)
+                # ax32.set_title('Head Direction', color=color_pval(hd_p_val))
 
                 # ax42: place surrogates
                 ax42 = plt.subplot(grid[4, 2])
-                plot_surrogates(place_surrs, place_info, place_surr_p_val, ax=ax42)
-                ax42.set_title('Place Surrogates',
-                               color='red' if place_surr_p_val < 0.05 else 'black')
+                plot_surrogates(place_surrs, place_info, place_p_val, ax=ax42)
+                ax42.set_title('Place Surrogates', color=color_pval(place_p_val))
 
                 # ax50: firing rates across trial segments
                 ax50 = plt.subplot(grid[5, 0])
                 plot_bar(sp_all_frs.mean(0), [0, 1, 2, 3], yerr=sem(sp_all_frs, 0), ax=ax50,
                          title='Serial Position')
-                for ind, p_val in enumerate(sp_surr_p_vals):
+                for ind, p_val in enumerate(sp_p_vals):
                     if p_val < 0.05:
                         ax50.text(ind, ax50.get_ylim()[1]-0.15*ax50.get_ylim()[1],
                                 '*', c='red', fontdict={'fontsize' : 25}, ha='center')
@@ -371,13 +366,12 @@ def main():
                 # ax51: spatial target firing
                 ax51 = plt.subplot(grid[5, 1])
                 plot_heatmap(target_bins, transpose=True, ax=ax51,
-                             title='Range: {:1.2f}-{:1.2f}'.format(np.nanmin(target_bins), np.nanmax(target_bins)))
+                             title='Range: {:1.2f}-{:1.2f}'.format(*get_range(target_bins)))
 
                 # ax52: target surrogates
                 ax52 = plt.subplot(grid[5, 2])
-                plot_surrogates(target_surrs, target_info, target_surr_p_val, ax=ax52)
-                ax52.set_title('Target Surrogates',
-                               color='red' if target_surr_p_val < 0.05 else 'black')
+                plot_surrogates(target_surrs, target_info, target_p_val, ax=ax52)
+                ax52.set_title('Target Surrogates', color=color_pval(target_p_val))
 
                 # Add super title to the report
                 suptitle = 'Unit Report: {}-U{}'.format(session_id, unit_ind)
@@ -407,24 +401,24 @@ def main():
                 results['fr_t_val_full'] = fr_t_val_full
                 results['fr_p_val_full'] = fr_p_val_full
                 results['place_info'] = place_info
-                results['place_p_val'] = place_surr_p_val
+                results['place_p_val'] = place_p_val
                 results['place_z_score'] = place_z_score
                 results['target_info'] = target_info
-                results['target_p_val'] = target_surr_p_val
+                results['target_p_val'] = target_p_val
                 results['target_z_score'] = target_z_score
-                results['sp_p_val_0'] = sp_surr_p_vals[0]
-                results['sp_p_val_1'] = sp_surr_p_vals[1]
-                results['sp_p_val_2'] = sp_surr_p_vals[2]
-                results['sp_p_val_3'] = sp_surr_p_vals[3]
+                results['sp_p_val_0'] = sp_p_vals[0]
+                results['sp_p_val_1'] = sp_p_vals[1]
+                results['sp_p_val_2'] = sp_p_vals[2]
+                results['sp_p_val_3'] = sp_p_vals[3]
                 results['sp_z_score_0'] = sp_z_scores[0]
                 results['sp_z_score_1'] = sp_z_scores[1]
                 results['sp_z_score_2'] = sp_z_scores[2]
                 results['sp_z_score_3'] = sp_z_scores[3]
 
-                # results['hd_z_val'] = hd_z_val
-                # results['hd_p_val'] = hd_p_val
-                # results['hd_surr_p'] = hd_surr_p_val
-                # results['hd_surr_z'] = hd_z_score
+                # results['hd_zstat'] = hd_zstat
+                # results['hd_pstat'] = hd_pstat
+                # results['hd_p'] = hd_p_val
+                # results['hd_z'] = hd_z_score
 
                 # Save out unit results
                 save_json(results, name + '.json', folder=str(PATHS['RESULTS'] / 'units' / TASK))
