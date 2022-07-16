bb2eb18e145a27ae6f1f26f05b84501639a15ce6
Tom Donoghue <tdonoghue.research@gmail.com>
2022-07-16 15:08:33 -0400

    consolidate model code

---
+++ code/models.py
@@ -0,0 +1,41 @@
+""""Helper code & functions to define analysis models."""
+
+from functools import partial
+
+from spiketools.stats.anova import create_dataframe_bins, fit_anova
+
+###################################################################################################
+###################################################################################################
+
+## PLACE MODELS
+
+PLACE = {
+    'MODEL' : 'fr ~ C(bin)',
+    'FEATURE' : 'C(bin)',
+    'COLUMNS' : ['bin', 'fr']
+}
+
+create_df_place = partial(create_dataframe_bins, columns=PLACE['COLUMNS'])
+fit_anova_place = partial(fit_anova, formula=PLACE['MODEL'], feature=PLACE['FEATURE'])
+
+## SPATIAL TARGET MODELS
+
+TARGET = {
+    'MODEL' : 'fr ~ C(target_bin)',
+    'FEATURE' : 'C(target_bin)',
+    'COLUMNS' : ['target_bin', 'fr']
+}
+
+fit_anova_target = partial(fit_anova, formula=TARGET['MODEL'], feature=TARGET['FEATURE'])
+
+## SERIAL POSITION MODELS
+
+SERIAL_POSITION = {
+    'MODEL' : 'fr ~ C(segment)',
+    'FEATURE' : 'C(segment)',
+    'COLUMNS' : ['segment', 'fr']
+}
+
+create_df_serial = partial(create_dataframe_bins, columns=SERIAL_POSITION['COLUMNS'])
+fit_anova_serial = partial(fit_anova, formula=SERIAL_POSITION['MODEL'],
+                           feature=SERIAL_POSITION['FEATURE'])
