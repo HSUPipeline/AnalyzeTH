e1979b5b1f30bd5a9a32b050119c4429bf5f089e
Tom Donoghue <tdonoghue.research@gmail.com>
2022-07-17 15:49:13 -0400

    interim update for settings

--- scripts/unit_analysis.py
+++ scripts/unit_analysis.py
@@ -1,6 +1,5 @@
 """Run TH analysis across all units."""
 
-import warnings
 import traceback
 
 import numpy as np
@@ -16,7 +15,7 @@ from spiketools.measures import compute_isis
 from spiketools.stats.shuffle import shuffle_spikes
 from spiketools.stats.trials import compare_pre_post_activity
 from spiketools.plts.spikes import plot_isis
-from spiketools.plts.spatial import plot_positions, plot_heatmap
+from spiketools.plts.spatial import plot_positions, plot_heatmap, create_heat_title
 from spiketools.plts.trials import plot_rasters, create_trial_title
 from spiketools.plts.data import plot_bar, plot_polar_hist, plot_text
 from spiketools.plts.stats import plot_surrogates
@@ -26,14 +25,13 @@ from spiketools.stats.permutations import compute_surrogate_stats
 from spiketools.spatial.occupancy import (compute_occupancy, compute_bin_edges,
                                           compute_bin_assignment, compute_bin_firing)
 from spiketools.spatial.information import compute_spatial_information
-from spiketools.utils.data import compute_range
 from spiketools.utils.extract import get_values_by_times
-from spiketools.utils.trials import (epoch_spikes_by_event, epoch_spikes_by_range,
-                                     epoch_data_by_range)
+from spiketools.utils.epoch import (epoch_spikes_by_event, epoch_spikes_by_range,
+                                    epoch_data_by_range)
 from spiketools.utils.base import select_from_list
 
 # Import settings from local file
-from settings import TASK, PATHS, IGNORE, UNITS, METHODS, ANALYSES, SURROGATES
+from settings import TASK, PATHS, IGNORE, UNITS, METHODS, BINS, OCCUPANCY, WINDOWS, SURROGATES
 
 # Import local code
 import sys
@@ -179,27 +177,24 @@ def main():
 
                 # Compute firing related to chest presentation
                 all_chests = epoch_spikes_by_event(spikes, np.concatenate(chest_openings),
-                                                   ANALYSES['TRIAL_RANGE'])
+                                                   WINDOWS['trial_range'])
                 empty_trials = select_from_list(all_chests, empty_mask)
                 full_trials = select_from_list(all_chests, full_mask)
 
                 # Compute bin edges
                 x_bin_edges, y_bin_edges = compute_bin_edges(\
-                    positions, ANALYSES['PLACE_BINS'], area_range=area_range)
+                    positions, BINS['place'], area_range=area_range)
 
                 # Get position values for each spike
                 spike_positions = get_values_by_times(ptimes, positions, spikes, threshold=0.25)
 
                 # Compute occupancy
-                occ_kwargs = {'minimum' : ANALYSES['MIN_OCCUPANCY'],
-                              'area_range' : area_range, 'set_nan' : True}
-                occ = compute_occupancy(positions, ptimes,
-                                        ANALYSES['PLACE_BINS'],
-                                        speed, **occ_kwargs)
+                occ = compute_occupancy(positions, ptimes, BINS['place'], area_range,
+                                        speed, **OCCUPANCY)
 
                 # Compute spatial bin assignments & binned firing, and normalize by occupancy
                 x_binl, y_binl = compute_bin_assignment(spike_positions, x_bin_edges, y_bin_edges)
-                bin_firing = compute_bin_firing(ANALYSES['PLACE_BINS'], x_binl, y_binl)
+                bin_firing = compute_bin_firing(BINS['place'], x_binl, y_binl)
                 bin_firing = bin_firing / occ
 
                 # Get head direction for each spike
@@ -207,7 +202,7 @@ def main():
 
                 # Compute edges for chest binning
                 ch_x_edges, ch_y_edges = compute_bin_edges(\
-                    positions, ANALYSES['CHEST_BINS'], area_range=area_range)
+                    positions, BINS['chest'], area_range=area_range)
 
                 # Assign each chest to a bin
                 chest_pos = np.array([chest_xs, chest_ys])
@@ -218,23 +213,23 @@ def main():
                 ch_ybin = ch_ybin - 1
 
                 # Compute chest occupancy
-                chest_occupancy = compute_bin_firing(ANALYSES['CHEST_BINS'], ch_xbin, ch_ybin)
+                chest_occupancy = compute_bin_firing(BINS['chest'], ch_xbin, ch_ybin)
 
                 ## STATISTICS
 
                 # Compare pre/post chest events, computing firing rates & t-test comparison
                 fr_pre_all, fr_post_all, results['fr_t_val_all'], results['fr_p_val_all'] = \
-                    = compare_pre_post_activity(all_chests, ANALYSES['PRE'], ANALYSES['POST'])
+                    = compare_pre_post_activity(all_chests, WINDOWS['pre'], WINDOWS['post'])
                 fr_pre_empt, fr_post_empt, results['fr_t_val_empt'], results['fr_p_val_empt'] = \
-                    = compare_pre_post_activity(empty_trials, ANALYSES['PRE'], ANALYSES['POST'])
+                    = compare_pre_post_activity(empty_trials, WINDOWS['pre'], WINDOWS['post'])
                 fr_pre_full, fr_post_full, results['fr_t_val_full'], results['fr_p_val_full'] = \
-                    = compare_pre_post_activity(full_trials, ANALYSES['PRE'], ANALYSES['POST'])
+                    = compare_pre_post_activity(full_trials, WINDOWS['pre'], WINDOWS['post'])
 
                 # Place cell analysis
                 if METHODS['PLACE'] == 'INFO':
                     results['place_info'] = compute_spatial_information(bin_firing, occ)
                 if METHODS['PLACE'] == 'ANOVA':
-                    place_trial = get_trial_place(spikes, nwbfile.trials, ANALYSES['PLACE_BINS'],
+                    place_trial = get_trial_place(spikes, BINS['bins'], nav_starts, nav_stops,
                                                   ptimes, positions, speed, x_bin_edges, y_bin_edges,
                                                   occ_kwargs)
                     results['place_anova'] = fit_anova_place(create_df_place(place_trial, drop_na=True))
@@ -242,12 +237,12 @@ def main():
                 # Target cell analysis
                 target_bins = compute_spatial_target_bins(\
                     spikes, nav_starts, chest_openings, chest_trials, ptimes, positions,
-                    ANALYSES['CHEST_BINS'], ch_xbin, ch_ybin)
+                    BINS['chest'], ch_xbin, ch_ybin)
                 if METHODS['TARGET'] == 'INFO':
                     results['target_info'] = compute_spatial_information(target_bins, chest_occupancy)
 
                 if METHODS['TARGET'] == 'ANOVA':
-                    target_trial = get_trial_target(spikes, nav_starts, ANALYSES['CHEST_BINS'],
+                    target_trial = get_trial_target(spikes, nav_starts, BINS['chest'],
                                                     chest_openings, chest_trials, ch_xbin, ch_ybin,
                                                     ptimes, positions)
                     results['target_anova'] = fit_anova_target(create_df_target(target_trial))
@@ -262,42 +257,38 @@ def main():
                 ## SURROGATES
 
                 # Create shuffled time series for comparison
-                times_shuffle = shuffle_spikes(spikes,
-                                               SURROGATES['SHUFFLE_APPROACH'],
-                                               SURROGATES['N_SURROGATES'])
+                times_shuffle = shuffle_spikes(spikes, SURROGATES['approach'], SURROGATES['n_shuffles'])
 
                 # Initialize to store surrogate analysis outputs
-                surrs = {analysis : np.zeros(SURROGATES['N_SURROGATES']) for analysis in surr_analyses}
+                surrs = {analysis : np.zeros(SURROGATES['n_shuffles']) for analysis in surr_analyses}
 
                 for ind, shuffle in enumerate(times_shuffle):
 
                     # PLACE
-                    if METHODS['PLACE'] == 'INFO':
-                        surr_bin_firing = compute_place_bins(stimes, bins, ptimes, positions,
+                    if METHODS['place'] == 'info':
+                        surr_bin_firing = compute_place_bins(stimes, BINS['place'], ptimes, positions,
                                                              x_bin_edges, y_bin_edges)
                         surrs['place_info'][ind] = compute_spatial_information(bin_firing, occ, normalize=True)
-                    if METHODS['PLACE'] == 'ANOVA':
-                        s_place_trial = get_trial_place(shuffle, nwbfile.trials, ANALYSES['PLACE_BINS'],
+                    if METHODS['place'] == 'anova':
+                        s_place_trial = get_trial_place(shuffle, BINS['place'], nav_starts, nav_stops,
                                                         ptimes, positions, speed, x_bin_edges, y_bin_edges,
                                                         occ_kwargs)
                         surrs['place_anova'][ind] = fit_anova_place(create_df_place(s_place_trial, drop_na=True))
 
                     # TARGET
-                    if METHODS['TARGET'] == 'INFO':
+                    if METHODS['target'] == 'info':
                         s_target_bins = compute_spatial_target_bins(\
                             shuffle, nav_starts, chest_openings, chest_trials, ptimes, positions,
-                            ANALYSES['CHEST_BINS'], ch_xbin, ch_ybin)
+                            BINS['chest'], ch_xbin, ch_ybin)
                         surrs['target_info'][ind] = compute_spatial_information(s_target_bins, chest_occupancy)
-                    if METHODS['TARGET'] == 'ANOVA':
+                    if METHODS['target'] == 'anova':
                         s_target_trial = get_trial_target(\
-                            shuffle, nav_starts, ANALYSES['CHEST_BINS'], chest_openings, chest_trials,
+                            shuffle, nav_starts, BINS['chest'], chest_openings, chest_trials,
                             ch_xbin, ch_ybin, ptimes, positions)
                         surrs['target_anova'][ind] = fit_anova_target(create_df_target(s_target_trial))
 
                     # SERIAL POSITION
-                    sp_all_frs = compute_segment_frs(shuffle, serial_seg_times)
-                    s_sp_all_frs = compute_serial_position_fr(\
-                        shuffle, nav_starts, chest_openings, chest_trials)
+                    s_sp_all_frs = compute_segment_frs(shuffle, serial_seg_times)
                     surrs['sp_anova'][ind] = fit_anova_serial(create_df_serial(s_sp_all_frs))
 
                     # HEAD DIRECTION
@@ -330,7 +321,7 @@ def main():
                 plot_isis(compute_isis(spikes), bins=100, range=(0, 2), ax=get_grid_subplot(grid, 0, 2))
 
                 # 10: chest related firing
-                plot_rasters(all_chests, xlim=ANALYSES['TRIAL_RANGE'], vline=0,
+                plot_rasters(all_chests, xlim=ANALYSES['trial_range'], vline=0,
                              title=create_trial_title('All Chests', \
                                 fr_pre_all, fr_post_all, fr_t_val_all, results['fr_p_val_all']),
                              title_color=color_pval(results['fr_p_val_all']),
@@ -338,15 +329,12 @@ def main():
 
 
                 # 12&22: Compare Empty & Full chest trials
-                # Empty chest trials
-                plot_rasters(empty_trials, xlim=ANALYSES['TRIAL_RANGE'], vline=0,
+                plot_rasters(empty_trials, xlim=ANALYSES['trial_range'], vline=0,
                              title=create_trial_title('Empty', \
                                 fr_pre_empt, fr_post_empt, fr_t_val_empt, results['fr_p_val_empt']),
                              title_color=color_pval(results['fr_p_val_empt']), title_fontsize=14,
                              ax=get_grid_subplot(grid, 1, 2))
-
-                # Full chest trials
-                plot_rasters(full_trials, xlim=ANALYSES['TRIAL_RANGE'], vline=0,
+                plot_rasters(full_trials, xlim=ANALYSES['trial_range'], vline=0,
                              title=create_trial_title('Full', \
                                 fr_pre_full, fr_post_full, fr_t_val_full, results['fr_p_val_full']),
                              title_color=color_pval(results['fr_p_val_full']), title_fontsize=14,
@@ -358,8 +346,8 @@ def main():
                                ax=get_grid_subplot(grid, slice(3, 5), 0))
 
                 # ax31: positional heatmap
-                plot_heatmap(bin_firing, transpose=True, ax=get_grid_subplot(grid, slice(3, 5), 1),
-                             title='Range: {:1.2f}-{:1.2f}'.format(*compute_range(bin_firing)))
+                plot_heatmap(bin_firing, ax=get_grid_subplot(grid, slice(3, 5), 1),
+                             title=create_heat_title('Bin Firing', bin_firing))
 
                 # ax32: head direction of spike firing
                 # plot_polar_hist(spike_hds, xticklabels=[], yticklabels=[],
@@ -384,9 +372,8 @@ def main():
                          ax=get_grid_subplot(grid, 5, 0))
 
                 # ax51: spatial target firing
-                plot_heatmap(target_bins, transpose=True,
-                             title='Range: {:1.2f}-{:1.2f}'.format(*compute_range(target_bins)),
-                             ax=get_grid_subplot(grid, 5, 1))
+                plot_heatmap(target_bins, ax=get_grid_subplot(grid, 5, 1),
+                             title=create_heat_title('Spatial Target', target_bins))
 
                 # ax52: target surrogates
                 plot_surrogates(surrs['target_anova'], results['target_anova'],
