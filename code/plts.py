51d117f3f6d82f11d0b2947cf662e7436daf715b
claire98han <zh2497@columbia.edu>
2022-09-12 16:40:51 -0400

    add behavioral analysis

--- code/plts.py
+++ code/plts.py
@@ -3,6 +3,7 @@
 from matplotlib import gridspec
 from spiketools.plts.task import plot_task_structure as _plot_task_structure
 from spiketools.plts.trials import plot_rasters
+from spiketools.plts.data import plot_bar, plot_hist
 from spiketools.plts.spatial import create_heat_title
 from spiketools.plts.utils import check_ax, savefig, set_plt_kwargs
 from spiketools.plts.annotate import _add_vlines, _add_box_shades, _add_hlines
@@ -81,3 +82,109 @@ def plot_spikes_trial(spikes, spikes_trial, nav_stops_trial, nav_spikes_all, nav
     ax3.set(yticks=[])
 
     fig.suptitle(create_heat_title('{}'.format(name), frs), fontsize=20)
+    
+    
+def plot_distance_error(norm_error, norm_error_THF, norm_error_THO):
+    """Plot the normalized distance error across sessions"""
+    
+    fig=plt.figure(figsize=(16, 3))
+    plt.subplots_adjust(wspace=0.4)
+    ax = plt.subplot(132)
+    ax1 = plt.subplot(133)
+    ax2 = plt.subplot(131)
+
+    plot_hist(norm_error, color='silver', ax=ax2)
+    _add_vlines(0.5, linestyle='--', color='black', ax=ax2)
+    ax2.set(title='All recall distance error', xlabel='Mean normalized \n distance error', ylabel='Number of sessions')
+    ax2.set_xticks([0, 0.2, 0.4, 0.6, 0.8, 1])
+
+    plot_hist(norm_error_THF, color='silver', ax=ax)
+    _add_vlines(0.5, linestyle='--', color='black', ax=ax)
+    ax.set(title='THF recall distance error', xlabel='Mean normalized \n distance error', ylabel='Number of sessions')
+    ax.set_yticks(range(3))
+    ax.set_xticks([0, 0.2, 0.4, 0.6, 0.8, 1])
+
+    plot_hist(norm_error_THO, color='silver', ax=ax1)
+    _add_vlines(0.5, linestyle='--', color='black', ax=ax1)
+    ax1.set(title='THO recall distance error', xlabel='Mean normalized \n distance error', ylabel='Number of sessions')
+    ax1.set_xticks([0, 0.2, 0.4, 0.6, 0.8, 1])
+
+    ax.spines.right.set_visible(False)
+    ax.spines.top.set_visible(False)
+    ax.yaxis.set_ticks_position('left')
+    ax.xaxis.set_ticks_position('bottom')
+    ax1.spines.right.set_visible(False)
+    ax1.spines.top.set_visible(False)
+    ax1.yaxis.set_ticks_position('left')
+    ax1.xaxis.set_ticks_position('bottom')
+    ax2.spines.right.set_visible(False)
+    ax2.spines.top.set_visible(False)
+    ax2.yaxis.set_ticks_position('left')
+    ax2.xaxis.set_ticks_position('bottom')
+    
+
+def plot_recall_correctness(correct_all, correct_THF, correct_THO):
+    """Plot the percentage of recall correctness across sessions"""
+    
+    fig=plt.figure(figsize=(16, 3))
+    plt.subplots_adjust(wspace=0.4)
+    ax1 = plt.subplot(131)
+    ax2 = plt.subplot(132)
+    ax3 = plt.subplot(133)
+
+    plot_hist(correct_all, color='silver', ax=ax1)
+    plot_hist(correct_THF, color='silver', ax=ax2)
+    plot_hist(correct_THO, color='silver', ax=ax3)
+
+    _add_vlines(50, linestyle='--', color='black', ax=ax1)
+    _add_vlines(50, linestyle='--', color='black', ax=ax2)
+    _add_vlines(50, linestyle='--', color='black', ax=ax3)
+    ax2.set_yticks(range(3))
+
+    ax1.set(title='All recall correctness', xlabel='% Recall', ylabel='Number of sessions')
+    ax2.set(title='THF recall correctness', xlabel='% Recall', ylabel='Number of sessions')
+    ax3.set(title='THO recall correctness', xlabel='% Recall', ylabel='Number of sessions')
+
+    ax3.spines.right.set_visible(False)
+    ax3.spines.top.set_visible(False)
+    ax3.yaxis.set_ticks_position('left')
+    ax3.xaxis.set_ticks_position('bottom')
+    ax1.spines.right.set_visible(False)
+    ax1.spines.top.set_visible(False)
+    ax1.yaxis.set_ticks_position('left')
+    ax1.xaxis.set_ticks_position('bottom')
+    ax2.spines.right.set_visible(False)
+    ax2.spines.top.set_visible(False)
+    ax2.yaxis.set_ticks_position('left')
+    ax2.xaxis.set_ticks_position('bottom')
+
+
+def plot_confidence_response(conf_all, conf_THF, conf_THO):
+    """Plot the number of confidence responsein each category across sessions"""
+    
+    fig=plt.figure(figsize=(16, 3))
+    plt.subplots_adjust(wspace=0.4)
+
+    ax1 = plt.subplot(131)
+    ax2 = plt.subplot(132)
+    ax3 = plt.subplot(133)
+
+    plot_bar(conf_all.values(), labels=conf_all.keys(), title='All confidence response',
+             ylabel='count', ax=ax1, color='silver')
+    plot_bar(conf_THF.values(), labels=conf_THF.keys(), title='THF confidence response', 
+             ylabel='count', ax=ax2, color='silver')
+    plot_bar(conf_THO.values(), labels=conf_THO.keys(), title='THO confidence response', 
+             ylabel='count', ax=ax3, color='silver')
+
+    ax3.spines.right.set_visible(False)
+    ax3.spines.top.set_visible(False)
+    ax3.yaxis.set_ticks_position('left')
+    ax3.xaxis.set_ticks_position('bottom')
+    ax1.spines.right.set_visible(False)
+    ax1.spines.top.set_visible(False)
+    ax1.yaxis.set_ticks_position('left')
+    ax1.xaxis.set_ticks_position('bottom')
+    ax2.spines.right.set_visible(False)
+    ax2.spines.top.set_visible(False)
+    ax2.yaxis.set_ticks_position('left')
+    ax2.xaxis.set_ticks_position('bottom')
--- code/utils.py
+++ code/utils.py
@@ -1,8 +1,11 @@
 """Utilities for working with Treasure Hunt data."""
 
+
 import numpy as np
 
+from convnwb.io import load_nwbfile
 from spiketools.utils.epoch import epoch_data_by_range
+from spiketools.utils.base import count_elements
 
 ###################################################################################################
 ###################################################################################################
@@ -22,3 +25,39 @@ def stack_trials(times_trials, values_trials):
     values = np.hstack(values_trials)
 
     return times, values
+
+def normalize_data(data):
+    """Helper function to normalize data into range of [0, 1]. """
+    return (data - np.min(data)) / (np.max(data) - np.min(data))
+
+def compute_distance_error(nwbfiles, data_folder):
+    """Compute the normalized distance error across sessions."""
+    
+    error_avg = np.zeros(len(nwbfiles))
+    for ind, nwbfile in enumerate(nwbfiles):
+        nwbfile, io = load_nwbfile(nwbfile, data_folder, return_io=True)
+        error = nwbfile.trials.error[:]
+        error_avg[ind] = np.mean(error)
+    error_normalized = normalize_data(error_avg)
+    
+    return error_normalized
+
+def compute_recall_percent(nwbfiles, data_folder):
+    """Compute the percentge of recall across sessions."""
+    
+    correct = np.zeros([len(nwbfiles)])
+    for ind, nwbfile in enumerate(nwbfiles):
+        nwbfile, io = load_nwbfile(nwbfile, data_folder, return_io=True)
+        correct[ind] = np.mean(nwbfile.trials.correct[:])*100
+        
+    return correct
+
+def get_confidence_response(nwbfiles, data_folder, labels):
+    """Count the confidence response in each category across sessions."""
+    for ind, nwbfile in enumerate(nwbfiles):
+        nwbfile, io = load_nwbfile(nwbfile, data_folder, return_io=True)
+        name = nwbfile.session_id
+        conf_counts = count_elements(nwbfile.trials.confidence_response.data[:],
+                                     labels=labels)
+    
+    return conf_counts
